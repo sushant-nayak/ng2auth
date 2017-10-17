6bd1fb0bb120ad674743c8c59b95511ec918fd9b
Sushant Nayak <sushant.nayak@live.com>
2017-10-17 15:17:50 -0400

    rolling back dependencies issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { UserComponent } from './components/user/user.component';
   imports: [
     BrowserModule
   ],
-  providers: [Express,Cors, BodyParser, Passport, Mongoose],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
